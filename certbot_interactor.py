c0221b6469e8e520adac58532a40e35693daacc8
Hugo RAVARD <hugo.ravard@veolia.com>
2024-04-02 22:21:51 +0200

    should have read the doc

--- certbot_interactor.py
+++ certbot_interactor.py
@@ -6,10 +6,9 @@ class certInt():
 
     def newCert(domains: list):
         for domain in domains:
-            subprocess.run('''certbot certonly \
-                            --dns-ovh \
-                            --dns-ovh-credentials ~/creds.ini \
-                            --dns-ovh-propagation-seconds 60 \
-                            -d '''+domain 
-            , timeout=70)
+            subprocess.run(["certbot" , "certonly", "--dns-ovh", 
+                            "--dns-ovh-credentials ~/creds.ini", 
+                            "--dns-ovh-propagation-seconds 60", 
+                            "-d " + domain] 
+                            , timeout=70)
             print("New cert for "+domain+" created")
