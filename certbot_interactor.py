9293fa285c9f28a73a4a208f6853c625b8d138ee
Hugo RAVARD <hugo.ravard@veolia.com>
2024-04-08 20:53:42 +0200

    sudo mandatory :/

--- certbot_interactor.py
+++ certbot_interactor.py
@@ -6,6 +6,6 @@ class certInt():
 
     def newCert(domains: list):
         for domain in domains:
-            subprocess.run(["certbot" , "certonly", "--dns-ovh --dns-ovh-credentials $HOME/creds.ini --dns-ovh-propagation-seconds 60 -d " + domain] 
+            subprocess.run(["sudo", "certbot" , "certonly", "--dns-ovh --dns-ovh-credentials $HOME/creds.ini --dns-ovh-propagation-seconds 60 -d " + domain] 
                             , timeout=70)
             print("New cert for "+domain+" created")
