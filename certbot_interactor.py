40002016b77c4bab316550ef36a5ec3abef574f3
Hugo RAVARD <hugo.ravard@veolia.com>
2024-04-08 21:07:06 +0200

    using shell in subprocess

--- certbot_interactor.py
+++ certbot_interactor.py
@@ -6,6 +6,6 @@ class certInt():
 
     def newCert(domains: list):
         for domain in domains:
-            subprocess.run(["sudo certbot certonly --dns-ovh --dns-ovh-credentials $HOME/creds.ini --dns-ovh-propagation-seconds 60 -d " + domain] 
-                            , timeout=70)
+            subprocess.run("sudo certbot certonly --dns-ovh --dns-ovh-credentials $HOME/creds.ini --dns-ovh-propagation-seconds 60 -d " + domain 
+                            , timeout=70, shell=True, check=True)
             print("New cert for "+domain+" created")
