9266de9c4701b8f08425c3b3a0e04b580dc7fc15
Hugo RAVARD <hugo.ravard@veolia.com>
2024-04-08 22:40:33 +0200

    switch to file usage for automation

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from certbot_interactor import *
 from sftp_mover import *
 
-domains = input("Insert domain names (comma separated) : ").split(",") # get domain names to work with
-sftp_param = input("Insert sftp parameters : hostname, username, privkey, port (comma separated) : ").split(",")
+#domains = input("Insert domain names (comma separated) : ").split(",") # get domain names to work with
+#sftp_param = input("Insert sftp parameters : hostname, username, privkey, port (comma separated) : ").split(",")
 
+param = open("param",'r').readlines()
+
+domains = param[1].split(",")
+sftp_param = param[2].split(",")
 certInt.newCert(domains)
 
 sftp_move = iLikeTo(sftp_param[0], sftp_param[1], sftp_param[2], sftp_param[3])
